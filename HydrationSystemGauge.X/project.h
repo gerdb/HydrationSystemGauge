0d8d1c3111c263dccb32e7555ddf15fb7bdaa4f2
gerdb <gerd@aline-gerd.de>
2022-01-05 17:13:55 +0100

    Project specific settings

---
+++ HydrationSystemGauge.X/project.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   project.h
+ * Author: gerd
+ *
+ * Created on January 5, 2022, 5:03 PM
+ */
+
+#ifndef PROJECT_H
+#define	PROJECT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// Enable AMP_TEST to measure every 100ms without sound output and sleep    
+//#define AMP_TEST
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PROJECT_H */
+
